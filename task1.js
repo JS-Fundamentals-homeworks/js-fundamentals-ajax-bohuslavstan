4788590cbea07b52761d1d1e9af5f13a0c162477
Bohuslav Stanishevskyy <bohuslav.stan@gmail.com>
2024-10-29 23:47:55 +0200

    Complete all tasks

--- task1.js
+++ task1.js
@@ -1,7 +1,24 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const usersList = document.querySelector("ul.usersList");
+
+async function fetchUsers() {
+  const req = await fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "GET",
+  });
+  const getUsersList = await req.json();
+  getUsersList.forEach((e) => {
+    const li = document.createElement("li");
+    usersList.appendChild(li);
+    li.textContent = e.name;
+  });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  fetchUsers();
+});
--- task2.js
+++ task2.js
@@ -5,3 +5,25 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const input = document.getElementById("userNameInput");
+const getUserBtn = document.getElementById("getUserButton");
+const output = document.getElementById("userCity");
+
+getUserBtn.addEventListener("click", () => {
+  const inputValue = input.value;
+  async function fetchUsers() {
+    const req = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "GET",
+    });
+    const getUsersList = await req.json();
+    getUsersList.forEach((e) => {
+      if (e.name === inputValue) {
+        console.log(e.name);
+        console.log(e.address.city);
+        output.textContent=e.address.city;
+      }
+    });
+  }
+    fetchUsers();
+});
